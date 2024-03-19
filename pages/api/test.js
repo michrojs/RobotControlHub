70791b9755ce11b87b13bc84b25ac5b709de0224
michr <michaelkaletin@yandex.ru>
2024-03-19 20:20:41 +0500

    Change to postgre sql

--- pages/api/test.js
+++
@@ -1,8 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-const id = 1;
-const { rows } = await sql`SELECT * FROM robots WHERE id = ${id};`;
-
-export default async (req, res) => {
-    res.status(200).json(rows);
-}
